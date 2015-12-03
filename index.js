b9f3ca4d38aea5f65e2e71b23dcd21a3fc43cdfc
Elana Kopelevich <ekopelevich@gmail.com>
2015-12-03 16:36:43 -0700

    logging 'responses' stuck on why it's not logging text from the modules. Taking a break

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
 var fatch = require('./fetch');
 
-var promise = fetch('./bears.txt');
+var promise = fetch('bears.txt');
 
 promise.then( function(bears) {
   console.log(bears);
-  return fetch('./fish.txt') {
+  return fetch('fish.txt');
 }).then( function( fish ) {
     console.log( fish );
-  });
 }).catch( function( err ) {
   console.error( 'ERROR!', err );
 });
@@ -15,3 +14,23 @@ promise.then( function(bears) {
 console.log( 'test' );
 
 document.body.style.backgroundColor = 'blue';
+
+// Promise.all([
+//   fetch('bears.txt'),
+//   fetch('fish.txt')
+// ]).then( function ( values ) {
+//   var bears = values[0];
+//   var fish = values[1];
+//   console.log( bears, fish );
+// });
+
+// var promise = new Promise( function ( resolve, reject ) {
+//   reject(new Error('another error'));
+//   resolve('All good');
+// });
+//
+// promise.then( function ( result ) {
+//   console.log('Was it good?', result );
+// }).catch( function( err ) {
+//   console.log('ERR', err);
+// });
